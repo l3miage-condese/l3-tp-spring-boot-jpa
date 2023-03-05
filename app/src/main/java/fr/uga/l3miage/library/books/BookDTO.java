e867b8603b40e9183ad3a7b3c901b5a14004701e
l3miage-condese <121897918+l3miage-condese@users.noreply.github.com>
2023-03-05 12:22:33 +0100

    Update BookDTO.java

--- app/src/main/java/fr/uga/l3miage/library/books/BookDTO.java
+++ app/src/main/java/fr/uga/l3miage/library/books/BookDTO.java
@@ -1,16 +1,24 @@
 package fr.uga.l3miage.library.books;
 
 import fr.uga.l3miage.library.authors.AuthorDTO;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 
 import java.util.Collection;
 
+import org.springframework.boot.context.properties.bind.DefaultValue;
+
 public record BookDTO(
-        Long id,
-        String title,
-        long isbn,
-        String publisher,
-        short year,
-        String language,
-        Collection<AuthorDTO> authors
-) {
+        @NotBlank @NotNull Long id,
+        @NotBlank @NotNull String title,
+        @Digits(integer = 13, fraction = 0) @Min(value = 1000000000L) @Max(value = 9999999999999L) long isbn,
+        @NotBlank String publisher,
+        @Digits(integer = 4, fraction = 0) @Min(value = -9999) @Max(value = 9999) @NotNull short year,
+        @DefaultValue("french") @Pattern(regexp = "^(french|english)$") @NotBlank @NotNull String language,
+
+        Collection<AuthorDTO> authors) {
 }
